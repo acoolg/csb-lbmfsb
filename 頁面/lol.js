f55cda80179afbcc39a5842ca787e5f041f9850a
acoolg <117921108+acoolg@users.noreply.github.com>
2023-02-26 09:16:36 +0800

    lol

---
+++ 頁面/lol.js
@@ -0,0 +1,8 @@
+var word = document.getElementById(type)
+var button = addEventListener(btn)
+var chat = document.getElementById(y)
+function send() {
+    document.getElementById("type")
+    chat.innerHTML = chat.innerHTML + `
+    <h4>${chat.value}</h4>`; 
+  }
